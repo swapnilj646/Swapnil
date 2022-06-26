361b8c92cc49b4c44ea46ed6d4ec7cb4f162a2c6
swapnilj646 <108207774+swapnilj646@users.noreply.github.com>
2022-06-26 10:14:53 +0530

    Add files via upload

---
+++ Jupyter Notebook.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## My Jupyter Notebook on IBM Watson Studio
+
+# __Swapnil Jadhav__
+# 
+# I am a Student
+
+# _I am interested in data science because it is one of the broadest fields available for individuals with a mathematical or statistical background. There are plenty of open positions that require data science skills and the by uses of digital application it will be increases in the upcoming years._
+
+# #### The following code is for the addition of two numbers and there multiplication by manual input
+
+# In[2]:
+
+
+n1=int(input('Enter the First Number:'))
+n2=int(input('Enter the Second Number:'))
+print('The sum is',n1+n2)
+print('The product is',n1*n2)
+
+
+# __Listed below are the top new technology trends in 2022 by simplilearn.com__
+# 1. Artificial Intelligence - Machine Learning
+# 2. Robotic Process Automation (RPA)
+# 3. Edge Computing
+# 4. Quantum Computing
+# 5. Virtual Reality and Augmented Reality
+# 6. Block Chain
+# 7. Internet of Things (IOT)
+# 8. 5G
+# 9. Cyber Security
+# 
+# __Open the below link for more explanation__
+# 
+# https://www.simplilearn.com/top-technology-trends-and-jobs-article
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
